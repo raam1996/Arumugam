119c694214a56b902fb337d3d633fcbd1ab01b50
raam1996 <mukeshraam1996@gmail.com>
2023-01-20 12:19:43 +0530

    Added my project

---
+++ src/test/java/Arumugan.java
@@ -0,0 +1,15 @@
+
+public class Arumugan {
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			System.out.println("uhiuhrihf");
+			System.out.println("uhiuhrihf2");
+			System.out.println("uhiuhrihf3");
+
+		}
+
+	
+
+}
